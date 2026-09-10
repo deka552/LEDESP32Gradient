1aa696d62938c582fc552b9d93b3c52ad7815aed
TomS <deka552@gmail.com>
2026-09-10 08:30:05 +0300

    5metres leds 8br 800mA

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,44 @@
 #include "Arduino.h"
 #include <FastLED.h>
 
-#define LED_PIN    19
-#define NUM_LEDS   72   // 144/м × 0.5м
-#define BRIGHTNESS 128
-#define SPEED_MS   30
+#define LED_PIN       19
+#define POT_PIN       21
+#define LEDS_PER_SEC  72
+#define NUM_SECTIONS  10
+#define NUM_LEDS      (LEDS_PER_SEC * NUM_SECTIONS)
 
 CRGB leds[NUM_LEDS];
-uint8_t hue = 0;
-int pos = 0;
-bool forward = true;
-unsigned long lastMove = 0;
+
+const CRGB COLOR_WARM = CRGB(255, 147, 41);
+const CRGB COLOR_COOL = CRGB(200, 220, 255);
+
+int snakeToLogical(int i) {
+  int sec = i / LEDS_PER_SEC;
+  int pos = i % LEDS_PER_SEC;
+  if (sec % 2 == 1) pos = LEDS_PER_SEC - 1 - pos;
+  return sec * LEDS_PER_SEC + pos;
+}
 
 void setup() {
+  Serial.begin(115200);
   FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
-  FastLED.setBrightness(BRIGHTNESS);
+  FastLED.setBrightness(8);
+  pinMode(POT_PIN, INPUT);
+
+  for (int i = 0; i < NUM_LEDS; i++) {
+    int logical = snakeToLogical(i);
+    uint8_t frac = (logical * 255UL) / (NUM_LEDS - 1);
+    leds[i] = blend(COLOR_WARM, COLOR_COOL, frac);
+  }
+
+  FastLED.show();
 }
 
 void loop() {
-  unsigned long now = millis();
-  if (now - lastMove < SPEED_MS) return;
-  lastMove = now;
-
-  fadeToBlackBy(leds, NUM_LEDS, 80);
-
-  leds[pos] = CHSV(hue, 255, 255);
-  hue++;
-
-  pos += forward ? 1 : -1;
-  if (pos >= NUM_LEDS) { pos = NUM_LEDS - 2; forward = false; }
-  if (pos < 0)         { pos = 1;             forward = true;  }
-
-  FastLED.show();
+  // Раскомментируйте, когда подключите потенциометр на пин 21:
+  // int pot = analogRead(POT_PIN);
+  // uint8_t br = map(pot, 0, 4095, 2, 255);
+  // FastLED.setBrightness(br);
+   FastLED.show();
+   delay(50);
 }
