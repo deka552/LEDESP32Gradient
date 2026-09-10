4bb100f9ad311af18f005d0a905abce867a4786c
TomS <deka552@gmail.com>
2026-09-10 08:53:26 +0300

    Pot on 34

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,27 @@
 #include "Arduino.h"
-#include <FastLED.h>
+/*
+  AnalogReadSerial
 
-#define LED_PIN       19
-#define POT_PIN       21
-#define LEDS_PER_SEC  72
-#define NUM_SECTIONS  10
-#define NUM_LEDS      (LEDS_PER_SEC * NUM_SECTIONS)
+  Reads an analog input on pin 0, prints the result to the Serial Monitor.
+  Graphical representation is available using Serial Plotter (Tools > Serial Plotter menu).
+  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
 
-CRGB leds[NUM_LEDS];
+  This example code is in the public domain.
 
-const CRGB COLOR_WARM = CRGB(255, 147, 41);
-const CRGB COLOR_COOL = CRGB(200, 220, 255);
+  https://docs.arduino.cc/built-in-examples/basics/AnalogReadSerial/
+*/
 
-int snakeToLogical(int i) {
-  int sec = i / LEDS_PER_SEC;
-  int pos = i % LEDS_PER_SEC;
-  if (sec % 2 == 1) pos = LEDS_PER_SEC - 1 - pos;
-  return sec * LEDS_PER_SEC + pos;
-}
-
+// the setup routine runs once when you press reset:
 void setup() {
+  // initialize serial communication at 9600 bits per second:
   Serial.begin(115200);
-  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
-  FastLED.setBrightness(8);
-  pinMode(POT_PIN, INPUT);
-
-  for (int i = 0; i < NUM_LEDS; i++) {
-    int logical = snakeToLogical(i);
-    uint8_t frac = (logical * 255UL) / (NUM_LEDS - 1);
-    leds[i] = blend(COLOR_WARM, COLOR_COOL, frac);
-  }
-
-  FastLED.show();
 }
 
+// the loop routine runs over and over again forever:
 void loop() {
-  // Раскомментируйте, когда подключите потенциометр на пин 21:
-  // int pot = analogRead(POT_PIN);
-  // uint8_t br = map(pot, 0, 4095, 2, 255);
-  // FastLED.setBrightness(br);
-   FastLED.show();
-   delay(50);
+  // read the input on analog pin 0:
+  int sensorValue = analogRead(34);
+  // print out the value you read:
+  Serial.println(sensorValue);
+  delay(1);  // delay in between reads for stability
 }
